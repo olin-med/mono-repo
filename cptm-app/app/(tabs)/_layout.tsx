1f369e8ab80628d3ffba4e7d4adc1b500c9b1177
ItsVasconcellos <fernando.asv@uol.com.br>
2025-06-25 17:30:28 -0300

    fix: layout style

--- cptm-app/app/(tabs)/_layout.tsx
+++ cptm-app/app/(tabs)/_layout.tsx
@@ -18,13 +18,6 @@ export default function TabLayout() {
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
       }}>
       <Tabs.Screen
         name="index"
