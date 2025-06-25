c3938365940314e0cc4fa269070ea66362b875da
olin-med <olin.costa@sou.inteli.edu.br>
2025-06-25 14:45:50 -0300

    first commit

---
+++ linhas-estacoes.tsx
@@ -0,0 +1,10 @@
+// app/linhas-estacoes.tsx
+import { Text, View } from 'react-native';
+
+export default function LinhasEstacoes() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Linhas e estações</Text>
+    </View>
+  );
+}
